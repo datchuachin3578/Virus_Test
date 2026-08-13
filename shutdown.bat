e1f0efdf5c71beecb219130f766c8063d10630ee
dat3578 <102861651+datchuachin3578@users.noreply.github.com>
2026-08-13 11:52:48 +0700

    Add files via upload

---
+++ shutdown.bat
@@ -0,0 +1,10 @@
+@echo off
+echo !!! NGUY HIEM: THU NGHIEM LENH TATT MAY !!!
+echo Dang kich hoat lenh shutdown trong Sandbox...
+timeout /t 3
+
+shutdown /s /t 10 /c "May tinh cua ban se bi tat trong 10 giay!"
+
+echo.
+echo Da gui lenh shutdown. Xem Sandboxie xu ly ra sao...
+pause
